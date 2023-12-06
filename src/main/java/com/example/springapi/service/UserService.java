6d22008a4318f83812eef1fe01a4f4164b08f8da
cale <nicolas.calero1@gmail.com>
2023-12-06 11:44:31 -0500

    Passwords now stored as hashes

--- src/main/java/com/example/springapi/service/UserService.java
+++ src/main/java/com/example/springapi/service/UserService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.Optional;
 
@@ -31,11 +33,13 @@ public class UserService implements IUserService{
 
     @Transactional
     public void create(User user) {
+        user.setPassword(toHash(user.getPassword()));
         userDAO.save(user);
     }
 
     @Transactional
     public User update(User user) {
+        user.setPassword(toHash(user.getPassword()));
         return userDAO.update(user);
     }
 
@@ -43,5 +47,25 @@ public class UserService implements IUserService{
     @Override
     public void delete(int id) {
         userDAO.delete(id);
+    }
+
+    public String toHash(String stringToHash)
+    {
+        try{
+            MessageDigest md = MessageDigest.getInstance("SHA256");
+            md.update(stringToHash.getBytes());
+            byte[] result = md.digest();
+            StringBuilder sb = new StringBuilder();
+            for(byte b : result)
+            {
+                sb.append(String.format("%02x",b));
+            }
+
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e)
+        {
+            return "No such algo";
+        }
+
     }
 }
